fe136cc6b21d43d95453c35774c75babc4ede78a
MikeNats <mixalis.tsougranis@gmail.com>
2013-12-19 15:28:48 +0200

    Create unlock.php

---
+++ unlock.php
@@ -0,0 +1,19 @@
+<?php
+
+$username = 'mike';
+$password = 'tsou';
+
+$ch = curl_init("http://espa.attackcode.com/skata/skata.php");
+curl_setopt( $ch, CURLOPT_ENCODING, "UTF-8" );
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+curl_setopt($ch, CURLOPT_USERPWD, $username . ":" . $password);
+ curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+ $contents = curl_exec ($ch);
+curl_close($ch);
+
+echo( $contents);
+
+
+?>
